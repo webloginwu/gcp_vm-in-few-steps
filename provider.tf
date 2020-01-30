dcc5a1657e0d9aaf7fdc5acfd1efb9156666ebae
wujunrong <publicwu@163.com>
2020-01-30 14:22:42 +0800

    private project reation

--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("credentials.json")}"
-project = "ac-shared-playground"
-region = "us-central1"
+credentials = "${file("frank-test-k8s-Terraform.json")}"
+project = "frank-test-k8s"
+region = "us-east1"
 }
+
