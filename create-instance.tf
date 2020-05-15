7b2c6ff6270bf802f24ced25cabd692e3a9f0161
wujunrong <publicwu@163.com>
2020-05-15 18:29:04 +0800

    meta data sshkey

--- create-instance.tf
+++ create-instance.tf
@@ -20,7 +20,9 @@ resource "google_compute_instance" "default" {
 
     //metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
     metadata_startup_script =  "${file("/Users/frankwu/MyDir/git/code_example/config_file/os_init_install_prerequisite.sh")}"
-    
+    metadata = {
+    sshKeys = "frankwu:${file("~/.ssh/id_rsa.pub")}"
+    }
     // Apply the firewall rule to allow external IPs to access this instance
     tags = ["http-server"]
 }
