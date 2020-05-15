7aedb08892fcc53a36b097c668d0bfc60677f8a6
wujunrong <publicwu@163.com>
2020-05-15 12:49:11 +0800

    gcp init

--- create-instance.tf
+++ create-instance.tf
@@ -18,8 +18,8 @@ resource "google_compute_instance" "default" {
   }
 
     //metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
-    metadata_startup_script = "sudo apt-get update && sudo apt-get install git -y &&  mkdir -p ~/git &&  cd ~/git && git clone   https://github.com/webloginwu/code_example.git "
-
+    metadata_startup_script =  "${file("/Users/frankwu/MyDir/git/code_example/config_file/os_init_install_prerequisite.sh")}"
+    
     // Apply the firewall rule to allow external IPs to access this instance
     tags = ["http-server"]
 }
